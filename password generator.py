5c229a7dd4ee45c58d2ec8a482c7a04a2e9e8cb5
Arshit Angiras <arshits724@gmail.com>
2025-08-10 00:52:17 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+length=int(input("Enter the length of the password"))
+
+character=string.ascii_letters+string.digits+string.punctuation
+
+password=""
+for i in range (length):
+    password+=random.choice(character)
+print("Generated password is",password)
